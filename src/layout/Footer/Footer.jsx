26cadced6099ac98e84c505878a9f1e60b249968
dmitriyrotaenko <rotaenko.dmitriy@gmail.com>
2025-06-28 15:37:21 +0300

    Add contacts list and copy section

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,16 +1,48 @@
 import './Footer.scss';
+import Icon from '@/components/Icon';
+import Logo from '@/components/Logo';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__content">
-        <div className="footer__contacts">
-
-        </div>
+        <address className="footer__contacts">
+          <Logo />
+          <ul className="footer__contacts-list">
+            <li className="footer__contacts-item">
+              <Icon
+                name="letter"
+                hasFill
+                className="footer__contacts-icon"
+              />
+              <a href="mailto:hello@skillbridge.com" className="footer__contacts-link">
+                hello@skillbridge.com
+              </a>
+            </li>
+            <li className="footer__contacts-item">
+              <Icon
+                name="phone"
+                hasFill
+                className="footer__contacts-icon"
+              />
+              <a href="tel:1234567890" className="footer__contacts-link">
+                123-456-7890
+              </a>
+            </li>
+            <li className="footer__contacts-item">
+              <Icon
+                name="location"
+                hasFill
+                className="footer__contacts-icon"
+              />
+              Somewhere in the World
+            </li>
+          </ul>
+        </address>
       </div>
       <div className="footer__copyright">
         {/* TODO: Fix whitespaces in copyright text */}
-        &#169; <time dateTime="2025">2025</time> Skillbridge. All rights reserved.
+        &copy; <time dateTime="2025">2025</time> Skillbridge. All rights reserved.
       </div>
     </footer>
   );
